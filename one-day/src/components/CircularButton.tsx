d738e3a989ef932daa0c63ea863ee934528b2a5b
Aqudi <gjdigj145@gmail.com>
2021-09-24 02:01:54 +0900

    feat: Add ionic and react example

---
+++ one-day/src/components/CircularButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { IonButton } from '@ionic/react'
+
+import './CircularButton.css'
+
+const CircularButton: React.FC = () => <IonButton>Default</IonButton>
+
+export default CircularButton
--- one-day/src/pages/Home.tsx
+++ one-day/src/pages/Home.tsx
@@ -1,8 +1,40 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react'
+import ExploreContainer from '../components/ExploreContainer'
+import CircularButton from '../components/CircularButton'
+import ITodo from '../interfaces/ITodo'
+import './Home.css'
+
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<Array<ITodo>>()
+  const [todoTitleInput, setTodoTitleInput] = useState(
+    'test title input 입니다.'
+  )
+
+  useEffect(() => {
+    // Todo List를 가져오는 코드
+    axios.get('https://jsonplaceholder.typicode.com/todos').then((response) => {
+      setTodos(response.data)
+    })
+
+    // Todo Item을 만드는 코드
+    axios
+      .post('https://jsonplaceholder.typicode.com/todos', {
+        title: todoTitleInput,
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [])
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,9 +49,11 @@ const Home: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
+        <CircularButton />
+        {JSON.stringify(todos?.splice(0, 2))}
       </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
