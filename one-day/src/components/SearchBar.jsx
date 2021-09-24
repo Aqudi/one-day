995a4214a6f5507b3d2622275f65568729cbdd52
alexiskim66 <ehpark10t@gmail.com>
2021-09-25 02:28:12 +0900

    feat: homepage css

---
+++ one-day/src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { IonSearchbar } from '@ionic/react'
+import styled from 'styled-components';
+
+const SearchBar = styled(IonSearchbar)`
+    &::part(native) {
+        background-color: white;
+        border-radius: 10px;
+        width: 80%;
+        height: 70px;
+        text-align: center;
+        display: block;
+        margin: auto;
+        padding: 0px;
+    }
+`;
+
+export default SearchBar
--- one-day/src/pages/Home.jsx
+++ one-day/src/pages/Home.jsx
@@ -1,50 +1,86 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react'
-import ExploreContainer from '../components/ExploreContainer'
-import CircularButton from '../components/CircularButton'
+import React, { useState } from 'react';
 import './Home.css'
-
-import ApiClient from '../api/ApiClient'
-import { useEffect, useState } from 'react'
+import {
+  IonSegment, IonSegmentButton, IonSearchbar, IonFooter, IonIcon, IonItem, IonButton, IonHeader,
+  IonLabel, IonPage, IonTitle, IonToolbar, IonContent, IonSelect, IonSelectOption,
+  IonList, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent,
+} from '@ionic/react';
+import { pencil, pin, home, star, map, calendar, personCircle, informationCircle } from 'ionicons/icons';
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
-  const [todos, setTodos] = useState([]);
-  const [todoTitleInput, setTodoTitleInput] = useState('test title input 입니다.')
-
-  useEffect(() => {
-    // Todo List를 가져오는 코드
-    ApiClient.get('https://jsonplaceholder.typicode.com/todos').then(response => {
-      setTodos(response.data)
-    })
-
-    // Todo Item을 만드는 코드
-    ApiClient
-      .post('https://jsonplaceholder.typicode.com/todos', {
-        title: todoTitleInput,
-      })
-      .then(response => {
-        console.log(response.data)
-      })
-  }, [])
-
-  return (
+  const [searchText, setSearchText] = useState('');
+return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-        <CircularButton>Hello</CircularButton>
-        {JSON.stringify(todos?.splice(0, 2))}
-      </IonContent>
-    </IonPage>
-  )
-}
+      <div className="head">
+          <div className="profileBox"><img src="./resources/icon.png"></img></div>
+          <p>안녕하세요 username님</p>
+          <SearchBar value={searchText} onIonChange={e => setSearchText(e.detail.value)}></SearchBar>
+        </div>
 
-export default Home
+      <div className="menuBar">
+        <IonToolbar>
+          <IonSegment onIonChange={e => console.log('Segment selected', e.detail.value)}>
+            <IonSegmentButton value="home">숙박
+              <IonIcon icon={home} />
+            </IonSegmentButton>
+            <IonSegmentButton value="bookmark">정보
+              <IonIcon icon={pin} />
+            </IonSegmentButton>
+            <IonSegmentButton value="map">현지인매칭
+            {/* <ion-icon name="contacts">ss</ion-icon> */}
+              <IonIcon icon={map} />
+            </IonSegmentButton>
+          </IonSegment>
+        </IonToolbar>
+      </div>
+
+  <div className='body'>
+
+    <div className="accommodation">
+    <IonCard>
+      <IonCardHeader>
+          <IonCardTitle>강원도 와화우 리조트</IonCardTitle>
+          <IonCardSubtitle>강원도 강릉시 와화우로</IonCardSubtitle>
+      </IonCardHeader>
+
+        <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+    </IonCard>
+
+    <IonCard>
+      <IonCardHeader>
+        <img id="homeImg"></img>
+          <IonCardTitle>강원도 와화우 리조트</IonCardTitle>
+          <IonCardSubtitle>강원도 강릉시 와화우로</IonCardSubtitle>
+          <IonCardSubtitle>강원도 강릉시 와화우로</IonCardSubtitle>
+      </IonCardHeader>
+
+        <IonCardContent>
+            Keep close to Nature's heart... and break clear away, once in awhile,
+            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+        </IonCardContent>
+    </IonCard>
+    </div>
+
+</div>
+
+      </IonContent>
+      <IonFooter>
+      <div className="underbar">
+        <IonButton color="primary" value="home">
+              <IonIcon icon={home} />
+        </IonButton>
+        <IonButton color="primary" value="pencil">
+              <IonIcon icon={pencil} />
+        </IonButton>
+      </div>
+      </IonFooter>
+    // </IonPage>
+  );
+};
+
+export default Home;
